b0ac6bd073ca7b1fccdabfcd0e27383932567d91
Sparsh Kesari <39815871+SparshKesari@users.noreply.github.com>
2021-08-19 19:50:36 +0530

    mark working for contraction
    Signed-off-by: Sparsh Kesari <39815871+SparshKesari@users.noreply.github.com>

--- grammar.js
+++ grammar.js
@@ -1,8 +1,8 @@
-const winkNLP = require('wink-nlp');
-const model = require('wink-eng-lite-web-model');
-const nlp = winkNLP(model);
-const its = nlp.its;
-const as = nlp.as;
+const winkNLP = require('wink-nlp')
+const model = require('wink-eng-lite-web-model')
+const nlp = winkNLP(model)
+const its = nlp.its
+const as = nlp.as
 
 /**
  * @param {text} text Input text
@@ -10,39 +10,21 @@ const as = nlp.as;
  */
 
 exports.checkContractions = (text) => {
-    const doc = nlp.readDoc(text);
-    // const tokens = doc.tokens();
-    const filteredTokens = doc.tokens().filter( (token) => (token.out(its.contractionFlag) === true) );
-    // console.log(tokens.out(its.value, as.array));
-    // console.log(tokens.out(its.contractionFlag, as.array));
-    // console.log(filteredTokens.out(its.value, as.array));
-    // console.log(filteredTokens.out(its.contractionFlag, as.value));
+  const doc = nlp.readDoc(text)
+  // const tokens = doc.tokens();
+  const filteredTokens = doc
+    .tokens()
+    .filter((token) => token.out(its.contractionFlag) === true)
+    
 
-    const contractions = filteredTokens.out(its.value, as.array);
-    console.log(contractions);
-    var count = 0;
-    for (var i in contractions) {
-      if (i % 2 !== 0 && contractions[i].charAt(1) !== '\'') {
-        count += 1;
-        console.log('Err!! ' + count);
-      }
+  const contractions = filteredTokens.out(its.value, as.array)
+  console.log(contractions)
+  var count = 0
+  for (var i in contractions) {
+    if (i % 2 !== 0 && contractions[i].charAt(1) !== "'") {
+      filteredTokens.each((t) => t.markup())
+      text = doc.out(its.markedUpText)
     }
-    return 'Hello';
-};
-
-  // const patterns = [
-  //   { name: 'space&punctuation', patterns: ['yes ,'] },
-  //   { name: 'cant', patterns: ['cant'] },
-  // ]
-  // const count = nlp.learnCustomEntities(patterns)
-  // const doc = nlp.readDoc(text)
-  // doc.customEntities().each((t) => t.markup())
-
-  // console.log(doc.out(its.markedUpText))
-  // text = doc.out(its.markedUpText)
-
-  // console.log(doc.tokens().out())
-  // console.log(doc.tokens().out(its.pos))
-  // console.log(doc.tokens().out(its.contractionFlag))
-
-  // return text
+  }
+  return text
+}
